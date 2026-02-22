af0d05696a75a6cdb5976b3e6a2fff3d363d38b0
jahnavi-gaur <121301783+jahnavi-gaur@users.noreply.github.com>
2026-02-22 10:32:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let btn = document.querySelector('.btn')
+let p = document.querySelector('p')
+
+btn.addEventListener('click',()=>{
+	let h1 = document.createElement('h1')
+	h1.innerText = "Entered Metaverse"
+	p.remove()
+	document.body.prepend(h1)
+})
